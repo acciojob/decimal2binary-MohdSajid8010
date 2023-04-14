bf12138db64cedd7e165dc8a6639c99165414d3a
MohdSajid8010 <117560964+MohdSajid8010@users.noreply.github.com>
2023-04-14 18:47:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function threeSum(arr, target) {
 // write your code here
   
+let p=1;
+let sum=0;
+while(n>0)
+{
+    let rem =n%2;
+    n=Math.floor(n/2);
+    
+    sum =sum+rem*p;
+    p=p*10;
+}
+	return sum;
 }
 
 module.exports = threeSum;
+
+
+// console.log(sum);
