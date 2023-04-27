90bd808bf9808891c34a0815148602e6c2f39bf7
MohdSajid8010 <117560964+MohdSajid8010@users.noreply.github.com>
2023-04-27 18:26:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 
 
 let p=1;
-let sum=0;
+let s="";
 while(n>0)
 {
     let rem =n%2;
     n=Math.floor(n/2);
-    
-    sum =sum+rem*p;
-    p=p*10;
+	
+if(rem == 1)
+{
+	s = s + '1';
+}
+else
+{
+ s = s+'0;
+}
+	
+	
+    // sum =sum+rem*p;
+    // p=p*10;
 }
 
-console.log(sum);
+console.log(s);
+
+
+
+// if(rem == '1')
+// 	s = s + '1';
+else s = s+'0
