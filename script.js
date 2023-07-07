0ecef934ad799ce7961f988bc7a448005705e559
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 15:15:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
-
-
-let p=1;
-let s="";
-while(n>0)
-{
-    let rem =n%2;
-    n=Math.floor(n/2);
-	
-if(rem == 1)
-{
-	s = s + '1';
-}
-else
-{
- s = s+'0;
-}
-	
-	
-    // s =s+rem*p;
-    // p=p*10;
+function decimalToBinary(num) {
+  // Write your code here
 }
 
-console.log(s);
-
-
-
-else s = s+'0
+window.decimalToBinary = decimalToBinary;
